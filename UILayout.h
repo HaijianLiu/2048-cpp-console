9b859c39113346ec124de80f06240fbb08fcae65
HaijianLiu <haijanliu@gmail.com>
2017-05-08 23:22:16 +0900

    add: PositionX() PositionY() GetScreenX() PrintScore(int x)

--- UILayout.h
+++ UILayout.h
@@ -16,12 +16,36 @@ public:
 		}
 	}
 
-	void PositionMainNumber(){
-		
+	// Print X of spaces
+	void PositionX(int x){
+		for (int i = 0; i < x; i++) {
+			printf(" ");
+		}
+	}
+
+	// Print Y of \n
+	void PositionY(int y){
+		for (int i = 0; i < y; i++) {
+			printf("\n");
+		}
+	}
+
+	// Get screenX to int
+	int GetScreenX(){
+		return screenX;
 	}
 
 	void UIGameOver(){
 		printf("GAME OVER\n");
+	}
+
+	void PrintScore(int x){
+		// printf("+---------------------------+\n");
+		// printf("|    SCORE          BEST    |\n");
+		// printf("|                           |\n");
+		// printf("+---------------------------+\n");
+		PositionX(x);
+		printf("JOIN THE NUMBERS AND GET TO 2048\n");
 	}
 
 private:
