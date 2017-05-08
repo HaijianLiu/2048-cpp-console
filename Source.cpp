9776d95d597c67e8d7ba20f2363594f3d0fbc75f
HaijianLiu <haijanliu@gmail.com>
2017-05-08 23:29:49 +0900

    1.change: Class Initiate to Global. 2.add: PrintScreen(). 3.optimize: if numbers didnt move, dont refresh screen.

--- Source.cpp
+++ Source.cpp
@@ -8,30 +8,40 @@
 #include "UILayout.h"
 #include "Game2048.h"
 
+// Class Initiate
+Controller controller;
+Game2048 game2048;
+UILayout uiLayout;
+
+void PrintScreen(){
+  uiLayout.PositionY(4);
+  uiLayout.PrintScore((uiLayout.GetScreenX() - 32 - 2)/2);
+  uiLayout.PositionY(2);
+  game2048.PrintNumber((uiLayout.GetScreenX() - 29 - 2)/2);
+}
 
 int main(void) {
   char direction; // For Values get from controller.GetDirection()
+  int x; // For Align Center Values
 
   // Random Seed Initiate
   srand((unsigned)time(NULL));
 
-  // Class Initiate
-  Controller controller;
-  Game2048 game2048;
-  UILayout uiLayout;
-
   // Set Start Numbers the Print them
   game2048.StartNumber();
-  game2048.PrintNumber();
+
+  PrintScreen();
 
   // Check if Game Over LOOP the Main Game
   while (!game2048.CheckGameOver()) {
     game2048.ResetIfMove();
     direction = controller.GetDirection();
     game2048.NumberCombine(direction);
-    if (game2048.GetIfMove()) game2048.AddNumber(direction);
-    system("cls");
-    game2048.PrintNumber();
+    if (game2048.GetIfMove()) {
+      game2048.AddNumber(direction);
+      system("cls");
+      PrintScreen();
+    }
   }
 
   uiLayout.UIGameOver();
